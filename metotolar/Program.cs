47234dc781d6eb809fc539a485b315ee4641c78b
Erhannnsoylu <soyluerhannn@gmail.com>
2024-07-01 15:55:37 +0300

    Proje dosyası ekle.

---
+++ metotolar/Program.cs
@@ -0,0 +1,37 @@
+﻿using metotolar;
+using System;
+    namespace Metotlar
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+           Urun urun1= new Urun();
+            urun1.Adi = "elma";
+            urun1.Fiyatı = "15";
+            urun1.Aciklama = "amasya elması";
+            Urun urun2 = new Urun();
+            urun1.Adi = "elmaA";
+            urun1.Fiyatı = "155";
+            urun1.Aciklama = "amasya elmasAAı";
+
+            Urun[] urunler = new Urun[] { urun1,urun2 };
+
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyatı);
+                Console.WriteLine(urun.Aciklama);
+            }
+
+            Console.WriteLine("------------METOTLAR-------------")
+            SepetManager  sepetManager= new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("armut","yesil","12",10);
+        }
+        
+    }
+}
---
+++ metotolar/SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metotolar
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun) {
+
+            Console.WriteLine("sepete eklendi" + urun.Adi)
+        }
+        public void Ekle2(string urunAdi, string aciklama, double Fiyatı, double StokAdedi,)
+        {
+            Console.WriteLine("cfsdvsvvsvds"+ urunAdi)
+        }
+    }
+}
+ 
